3d0c665cc061366691078ab782f6666d167d7567
Dani Dantas <dani@DaniVonRohr.local>
2019-03-16 14:39:06 -0400

    users page added

---
+++ data.js
@@ -0,0 +1,18 @@
+const data = {
+  name: "Daniela Dantas",
+  role: "Superadmin",
+
+  user1: {
+    name: "user",
+    role: "developer",
+    bio: "some bio info goes here",
+    bioPic: "dev.jpg"
+  },
+  user2: {
+    name: "user",
+    role: "designer",
+    bio: "some bio info goes here",
+    bioPic: "designer.jpg"
+  }
+};
+module.exports = data;
